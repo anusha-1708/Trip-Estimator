75ad1f4d34e85fd07bdd39e743bba628dabeee2b
anusha-shripati <anusha@logicrays.com>
2026-07-01 16:02:52 +0530

    Initial commit

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,9 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const url = await mongoose.connect(
-      `mongodb://127.0.0.1:27017/Trip-Database`,
-    );
+ 
+    const url = await mongoose.connect(process.env.MONGO_URI );
     console.log(`MongoDB  is connected: ${url.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error.message);
